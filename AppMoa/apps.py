e91f37f7558cdd9a8d2459c913edcaed90b69ff1
Samue587 <samuel17castilloo@gmail.com>
2026-05-26 12:36:06 -0500

    Agregar seeds automáticos

--- AppMoa/apps.py
+++ AppMoa/apps.py
@@ -2,11 +2,19 @@ from django.apps import AppConfig
 
 
 class AppmoaConfig(AppConfig):
-
     default_auto_field = 'django.db.models.BigAutoField'
-
     name = 'AppMoa'
 
     def ready(self):
 
-        import AppMoa.signals
+        try:
+            from .Seeds import crear_roles, crear_permisos, asignar_permisos
+
+            crear_roles()
+            crear_permisos()
+            asignar_permisos()
+
+            print("SEEDS EJECUTADOS")
+
+        except Exception as e:
+            print(e)
