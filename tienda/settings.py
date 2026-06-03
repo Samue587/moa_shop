3f62e974b0d66a867403834592af22e030150e33
Samue587 <samuel17castilloo@gmail.com>
2026-06-03 14:16:29 -0500

    Eliminar prueba SMTP manual

--- tienda/settings.py
+++ tienda/settings.py
@@ -176,7 +176,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # ══════════════════════════════════════════════════════
 EMAIL_BACKEND       = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST          = 'smtp-relay.brevo.com'
-EMAIL_PORT          = 587
+EMAIL_PORT          = 2525
 EMAIL_USE_TLS       = True
 EMAIL_TIMEOUT = 20
 EMAIL_HOST_USER     = os.getenv('EMAIL_HOST_USER', '')
